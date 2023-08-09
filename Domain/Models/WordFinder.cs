972162d7bc4aa458183a23d4b3961f51059885e5
Geo Zed <trabajo.codigo@gmail.com>
2023-08-09 19:25:48 -0300

    Implement Count Words method

---
+++ Domain/Models/WordFinder.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class WordFinder : IWordFinder
+    {
+        private IEnumerable<string> matrix;
+
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            this.matrix = matrix;
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            return CountWords(matrix, wordstream);
+        }
+
+        public static IEnumerable<string> CountWords(IEnumerable<string> wordsInput, IEnumerable<string> wordsToTest)
+        {
+
+            foreach (string wordToFind in wordsToTest)
+            {
+                foreach (string word in wordsInput)
+                {
+                    int count = CountOccurrences(word, wordToFind);
+
+                    if (count == 0)
+                        continue;
+
+                }
+            }
+
+            return null;
+        }
+
+        private static int CountOccurrences(string word, string wordToFind)
+        {
+            int count = 0, minIndex = word.IndexOf(wordToFind, 0);
+
+            while (minIndex != -1)
+            {
+                minIndex = word.IndexOf(wordToFind, minIndex + wordToFind.Length);
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
